346d45e3fa56e4633e2e87894c5ee652340bcd2c
Raghad <raghad.abu.rmeleh24@gmail.com>
2023-01-13 10:02:12 +0200

    first commit

---
+++ sportsProject/src/test/java/com/ragh/sportsProject/SportsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragh.sportsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
