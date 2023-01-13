3d3145601f4eaaf718fd15b675f23f141396296a
RaghadAbuRmeleh <118564285+RaghadAbuRmeleh@users.noreply.github.com>
2023-01-13 11:10:30 +0200

    This is TASK0 commit; Add hello wrold http call (#1)

--- sportsProject/src/main/java/com/ragh/sportsProject/SportsProjectApplication.java
+++ sportsProject/src/main/java/com/ragh/sportsProject/SportsProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SportsProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SportsProjectApplication.class, args);
 	}
 
